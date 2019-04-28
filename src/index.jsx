382b0a24a13b25390f8379e7cbcb0e5ed5fa2ef1
yuli hunsicker <yhunsicker@gmail.com>
2019-04-28 10:05:44 -0400

    react-to-do-list

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,123 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+const todoItems = [];
+todoItems.push({index: 1, value: "walk the dog", done: false});
+todoItems.push({index: 2, value: "grocery shopping", done: true});
+todoItems.push({index: 3, value: "plan vacation", done: true});
 
-const root = document.getElementById('root');
-if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+class TodoList extends React.Component {
+  render () {
+    const items = this.props.items.map((item, index) => {
+      return (
+        <TodoListItem key={index} item={item} index={index} removeItem={this.props.removeItem} markTodoDone={this.props.markTodoDone}/>
+      );
+    });
+    return (
+      <ul className="list-group"> {items} </ul>
+    );
+  }
 }
+
+class TodoListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onClickClose = this.onClickClose.bind(this);
+    this.onClickDone = this.onClickDone.bind(this);
+  }
+  onClickClose() {
+    const index = parseInt(this.props.index);
+    this.props.removeItem(index);
+  }
+  onClickDone() {
+    const index = parseInt(this.props.index);
+    this.props.markTodoDone(index);
+  }
+  render () {
+    var todoClass = this.props.item.done ?
+        "done" : "undone";
+    return(
+      <li className="list-group-item ">
+        <div className={todoClass}>
+          <span className="glyphicon glyphicon-ok icon" aria-hidden="true" onClick={this.onClickDone}></span>
+          {this.props.item.value}
+          <button type="button" className="close" onClick={this.onClickClose}>&times;</button>
+        </div>
+      </li>
+    );
+  }
+}
+
+class TodoForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  componentDidMount() {
+    this.refs.itemName.focus();
+  }
+  onSubmit(event) {
+    event.preventDefault();
+    const newItemValue = this.refs.itemName.value;
+
+    if(newItemValue) {
+      this.props.addItem({newItemValue});
+      this.refs.form.reset();
+    }
+  }
+  render () {
+    return (
+      <form ref="form" onSubmit={this.onSubmit} className="form-inline">
+        <input type="text" ref="itemName" className="form-control" placeholder="add a new todo..."/>
+        <button type="submit" className="btn btn-default">Add</button>
+      </form>
+    );
+  }
+}
+
+class TodoHeader extends React.Component {
+  render () {
+    return <h1>To Do List</h1>;
+  }
+}
+
+class TodoApp extends React.Component {
+  constructor (props) {
+    super(props);
+    this.addItem = this.addItem.bind(this);
+    this.removeItem = this.removeItem.bind(this);
+    this.markTodoDone = this.markTodoDone.bind(this);
+    this.state = {todoItems: todoItems};
+  }
+  addItem(todoItem) {
+    todoItems.unshift({
+      index: todoItems.length+1,
+      value: todoItem.newItemValue,
+      done: false
+    });
+    this.setState({todoItems: todoItems});
+  }
+  removeItem (itemIndex) {
+    todoItems.splice(itemIndex);
+    this.setState({todoItems: todoItems});
+  }
+  markTodoDone(itemIndex) {
+    const todo = todoItems[itemIndex];
+    todoItems.splice(itemIndex);
+    todo.done = !todo.done;
+    todo.done ? todoItems.push(todo) : todoItems.unshift(todo);
+    this.setState({todoItems: todoItems});
+  }
+  render() {
+    return (
+      <div id="main">
+        <TodoHeader />
+        <TodoList items={this.props.initItems} removeItem={this.removeItem} markTodoDone={this.markTodoDone}/>
+        <TodoForm addItem={this.addItem} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<TodoApp initItems={todoItems}/>, document.getElementById('app'));
